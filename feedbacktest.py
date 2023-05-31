4381c68b6bd3e3df5d1157341af0ceccff4ff86b
Samuel Yu <89748284+samuelcyu@users.noreply.github.com>
2023-05-31 14:29:43 -0500

    Feedback

--- feedbacktest.py
+++ feedbacktest.py
@@ -1,24 +1,54 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.conversation import Statement
 
-def provide_feedback(user_input, bot_response, is_good):
-    bot = ChatBot('MyBot')
-
-    if is_good:
-        bot.learn_response(bot_response, user_input)
-    else:
-        bot.learn_response('I apologize. I will try to improve.', user_input)
-
-# Create ChatBot instance
+# Create a ChatBot instance
 bot = ChatBot('MyBot')
 
-# Train the bot
-trainer = ChatterBotCorpusTrainer(bot)
-trainer.train("chatterbot.corpus.english")
+# Function to train the bot based on user feedback
+def train_bot(conversation, feedback):
+    # Get the user's input statement
+    user_input = conversation[-1]
 
-# Provide feedback
-user_input = "Hello"
-bot_response = bot.get_response(user_input)
-is_good = False
+    # Get the bot's response to the user's input
+    bot_response = bot.get_response(user_input)
 
-provide_feedback(user_input, bot_response.text, is_good)
+    # Check the feedback provided
+    if feedback:
+        # If the feedback is positive, mark the bot's response as correct
+        bot_response.confidence = 1.0
+
+        print("Thank you for your positive feedback!")
+    else:
+        # If the feedback is negative, prompt the user for an alternative response
+        new_response = input("Please provide an alternative response: ")
+
+        # Create a new statement with the alternative response
+        alternative_response = Statement(text=new_response)
+
+        # Replace the bot's response with the alternative response
+        conversation[-1] = alternative_response
+
+        print("Thank you for your feedback. The bot has been trained with an alternative response.")
+
+    # Train the bot with the conversation
+    bot.learn_response(user_input, bot_response)
+
+# Example usage
+user_input = input("User: ")
+conversation = []
+
+while user_input.lower() != 'exit':
+    # Append the user's input to the conversation
+    conversation.append(Statement(text=user_input))
+
+    # Get user feedback (1 for positive, 0 for negative)
+    feedback = int(input("Is the response good? (1 for yes, 0 for no): "))
+
+    # Train the bot based on the conversation and feedback
+    train_bot(conversation, feedback)
+
+    # Get the bot's response to the user's input
+    bot_response = bot.get_response(user_input)
+    print("Bot:", bot_response)
+
+    user_input = input("User: ")
