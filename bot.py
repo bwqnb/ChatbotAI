7f81b86754d9b5ffbb7eadcd2e88d22212ae47da
Samuel Yu <89748284+samuelcyu@users.noreply.github.com>
2023-06-02 15:45:48 -0500

    Com

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ chatbot = ChatBot(
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand.',
-            'maximum_similarity_threshold': 0.90,
+            'default_response': 'I am sorry, but I do not understand.', 
+            'maximum_similarity_threshold': 0.90, # accounts for some typos
             # 'response_selection_method': 'get_random_response'
 
         }
