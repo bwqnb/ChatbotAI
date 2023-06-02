276d988e6b5f7d6d5309fd7bead3c13f38ebad66
Samuel Yu <89748284+samuelcyu@users.noreply.github.com>
2023-06-02 09:29:26 -0500

    Feedback py should work now

--- feedbacktest.py
+++ feedbacktest.py
@@ -1,58 +1,48 @@
 from chatterbot import ChatBot
-from chatterbot.conversation import Statement
+from chatterbot.trainers import ListTrainer
 
 # Create a ChatBot instance
 bot = ChatBot('MyBot')
 
-# Function to train the bot based on user feedback
-def train_bot(conversation, feedback):
-    # Get the user's input statement
-    user_input = conversation[-1]
+# Create a ListTrainer
+trainer = ListTrainer(bot)
 
-    # Get the bot's response to the user's input
-    bot_response = bot.get_response(user_input)
-
-    # Check the feedback provided
-    if feedback:
-        # If the feedback is positive, mark the bot's response as correct
-        bot_response.confidence = 1.0
-        print(bot_response.confidence)
-        print("Thank you for your positive feedback!")
-    else:
-        # If the feedback is negative, prompt the user for an alternative response
-        new_response = input("Please provide an alternative response: ")
-
-        # Create a new statement with the alternative response
-        alternative_response = Statement(text=new_response)
-
-        # Replace the bot's response with the alternative response
-        conversation[-1] = alternative_response
-
-        print("Thank you for your feedback. The bot has been trained with an alternative response.")
-
-    # Train the bot with the conversation
-    bot.learn_response(user_input, bot_response)
-
-# Example usage
-user_input = input("User: ")
-conversation = []
-
-while user_input.lower() != 'exit':
-    # Append the user's input to the conversation
-    conversation.append(Statement(text=user_input))
-
-    # Get the bot's response to the user's input
-    bot_response = bot.get_response(user_input)
-    print("Bot:", bot_response) 
-
-
-    # Get user feedback (1 for positive, 0 for negative)
+# Function to get user feedback and weight
+def get_user_feedback():
     feedback = int(input("Is the response good? (1 for yes, 0 for no): "))
+    if feedback == 0:
+        weight = float(input("Enter the weight to influence the response (0.0 - 1.0): "))
+    else:
+        weight = 1.0
+    return feedback, weight
 
-    # Train the bot based on the conversation and feedback
-    train_bot(conversation, feedback)
-
-
+# Conversation loop
+while True:
+    # Get user input
     user_input = input("User: ")
 
+    # Get bot's response
+    bot_response = bot.get_response(user_input)
+    print("Bot:", bot_response)
 
+    # Prompt user for feedback and weight on the bot's response
+    feedback, weight = get_user_feedback()
+
+    if feedback == 0:
+        alternative_response = input("Please provide an alternative response: ")
+
+        # Train the bot with the user input and alternative response
+        trainer.train([
+            user_input,
+            alternative_response
+        ])
+
+        # Decrease confidence for wrong answer
+        bot_response.confidence *= weight
+
+    else:
+        # Increase confidence for correct answer
+        bot_response.confidence *= 1.0
+
+    if user_input.lower() == 'bye':
+        break
